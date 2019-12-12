d13c8f0c5804544af345a8fd9deaac400899f63c
Max Humber <max.humber@gmail.com>
2019-12-12 13:54:08 -0500

    thirsty

--- wrappers.py
+++ wrappers.py
@@ -1,5 +1,24 @@
-from quote import quote
+from quote import quote #pip install quote
 
-quote('shakespeare')
+quote('shakespeare', limit=1)
 
-!quote shakespeare
+# !quote shakespeare
+
+# !pip install urbandict
+#https://github.com/novel/py-urbandict
+import urbandict
+
+urbandict.define('Thirst Trap')
+
+# https://github.com/martin-majlis/Wikipedia-API/
+# !pip install wikipedia-api
+
+from wikipediaapi import Wikipedia
+wiki = Wikipedia('en')
+
+page = wiki.page('Python_(programming_language)')
+page.exists()
+page.text
+
+
+#
